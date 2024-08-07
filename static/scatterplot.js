5bcfe89ec4e773105bd9841581631c4266e1c7d5
Zhaorui Liu <378976087@qq.com>
2024-08-07 11:56:38 +0800

    Mouse interaction
    When the mouse passes by, the dots will enlarge.

--- static/scatterplot.js
+++ static/scatterplot.js
@@ -1,8 +1,8 @@
 // d3 gallery https://d3-graph-gallery.com/graph/scatter_basic.html
 // set the dimensions and margins of the graph
 const margin = {top: 10, right: 30, bottom: 30, left: 60},
-        width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+        width = 720 - margin.left - margin.right,
+        height = 640 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 const svg = d3.select("#my_dataviz")
@@ -51,8 +51,20 @@ function scatterplot_visualize(data) {
     .join("circle")
         .attr("cx", function (d) { return x(d.centroid); } )
         .attr("cy", function (d) { return y(d.rms); } )
-        .attr("r", 2)
+        .attr("r", 5)
         .style("fill", "#69b3a2")
         // .on("mouseover", highlight)
         // .on("mouseleave", doNotHighlight)
+        .on("mouseover", function(event, d) {
+            d3.select(this)
+              .transition()
+              .duration(100)
+              .attr("r", 10);
+        })
+        .on("mouseleave", function(event, d) {
+            d3.select(this)
+              .transition()
+              .duration(100)
+              .attr("r", 5);
+        })
 }
